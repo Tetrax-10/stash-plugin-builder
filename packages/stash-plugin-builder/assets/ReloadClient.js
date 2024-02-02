d4c8ef70fe0589ebad5be9c687d8a6f9fa1c5fa1
Raghavan <raghavanmarch2002@gmail.com>
2024-02-02 18:44:45 +0530

    format ReloadClient

--- packages/stash-plugin-builder/assets/ReloadClient.js
+++ packages/stash-plugin-builder/assets/ReloadClient.js
@@ -1,49 +1,49 @@
-(() => {
-  let socket;
+;(() => {
+    let socket
 
-  const state = {
-    connected: false,
-  };
-
-  function log(text, color) {
-    const colors = {
-      blue: "#61AFEF",
-      green: "#59CE8F",
-      red: "#D21312",
-    };
-
-    if (colors[color]) {
-      console.log(`%c${text}`, `color: ${colors[color]}`);
-    } else {
-      console.log(text);
+    const state = {
+        connected: false,
     }
-  }
 
-  socket = new WebSocket(`ws://${window.location.hostname}:8082`);
+    function log(text, color) {
+        const colors = {
+            blue: "#61AFEF",
+            green: "#59CE8F",
+            red: "#D21312",
+        }
 
-  socket.onerror = () => {
-    log("stash-plugin-builder is not watching", "blue");
-  };
-
-  socket.addEventListener("open", () => {
-    state.connected = true;
-  });
-
-  socket.addEventListener("close", () => {
-    if (state.connected) {
-      log("stash-plugin-builder lost connection to stash website!", "red");
-      state.connected = false;
+        if (colors[color]) {
+            console.log(`%c${text}`, `color: ${colors[color]}`)
+        } else {
+            console.log(text)
+        }
     }
-  });
 
-  socket.onmessage = (event) => {
-    switch (event.data) {
-      case "reload":
-        window.location.reload();
-        break;
-      case "connected":
-        log("stash-plugin-builder is watching for changes...", "green");
-        break;
+    socket = new WebSocket(`ws://${window.location.hostname}:8082`)
+
+    socket.onerror = () => {
+        log("stash-plugin-builder is not watching", "blue")
     }
-  };
-})();
+
+    socket.addEventListener("open", () => {
+        state.connected = true
+    })
+
+    socket.addEventListener("close", () => {
+        if (state.connected) {
+            log("stash-plugin-builder lost connection to stash website!", "red")
+            state.connected = false
+        }
+    })
+
+    socket.onmessage = (event) => {
+        switch (event.data) {
+            case "reload":
+                window.location.reload()
+                break
+            case "connected":
+                log("stash-plugin-builder is watching for changes...", "green")
+                break
+        }
+    }
+})()
